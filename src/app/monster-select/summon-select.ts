f60aa184aedf8ad8af13ff2f6de1d2bd0b9e9459
Isaac Careaga <aizakmori@gmail.com>
2025-07-19 18:52:46 -0300

    monstruos seleccionados ya se suman a la otra tabla, faltan detalles

--- src/app/monster-select/summon-select.ts
+++ src/app/monster-select/summon-select.ts
@@ -1,4 +1,4 @@
-import { Component, Input, output } from '@angular/core';
+import { Component} from '@angular/core';
 import { MonsterService } from '../monster-service';
 import { Monster } from '../interface/monster';
 
@@ -8,11 +8,16 @@ import { Monster } from '../interface/monster';
   templateUrl: './summon-select.html',
   styleUrl: './summon-select.scss'
 })
+/* suscribirse al patron observer */
 export class SummonSelect {
+  selected: Monster[] = [];
   constructor(private mservice: MonsterService){
     /*se da a entender que este mservice y el de summon-list es el mismo*/
+    //traemos los valores?
+    mservice._itemsSubject.subscribe((observable)=> this.selected = observable); 
   }
   removeElement(monster: Monster) {
+    console.log(this.selected);
     this.mservice.removeElement(monster);
   }
 }
--- src/app/monster-service.ts
+++ src/app/monster-service.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Monster } from './interface/monster';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 /* Logica de monstruos seleccionados
-*
-*
+  add de elementos
+  patron observer
+  uso de behaviorSubject
 */
 export class MonsterService {
-  selected: Monster[] = [];    /*inicio en vacio*/
+  private _selected: Monster[] = [];    //inicio en vacio con "_" por convencion
+  _itemsSubject: BehaviorSubject<Monster[]> = new BehaviorSubject(this._selected);   //hago observable la variable
+  public items: Observable<Monster[]> = this._itemsSubject.asObservable();
+  selected: any;
+
+  
   constructor(){}
 
   addElement(monster : Monster):void{
-    let aux = this.selected.find((v1) => v1.id == monster.id); /* funcion flecha para verificar si el elemento que envio ya esta en el carrito */
+    let aux = this._selected.find((v1) => v1.id == monster.id); /* funcion flecha para verificar si el elemento que envio ya esta en el carrito */
      if(!aux){  
-        this.selected.push({... monster});          /* de no existir creo un "clon" o copia del elemento con notacion funcional*/
+        this._selected.push({... monster});          /* de no existir creo un "clon" o copia del elemento con notacion funcional*/
      }else{
         aux.quantity += monster.quantity;     /*de existir el monstruo le sumo a la cantidad seleccionada deseada */
      }
-     console.log(this.selected);
+     console.log(this._selected);
+     this._itemsSubject.next(this._selected);         //emitimos el cambio del patron observer con .next() equivalente a .emit() en @output
   }
   removeElement(monster: Monster):void{
-    
+
   }
 }
